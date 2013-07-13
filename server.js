38899c89acbe321acda598e010fd787b6e6611ae
Saam Barati <saambarati1@gmail.com>
2013-07-13 16:59:56 -0700

    added file serving

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-
-var http = require('http')
+var restify = require('restify')
+  , server = restify.createServer()
+  , path = require('path')
+  , filed = require('filed')
   , port = 8081
+
+server.get('static/:filename', function (req, res) {
+  var filename = path.join(__dirname, 'static',  req.params.filename)
+  console.log('serving filename: ' + filename)
+  filed(filename).pipe(res)
+})
+
+server.get('/', function(req, res){
+  filed(path.join(__dirname, '/static/home.html')).pipe(res)
+})
+
+server.listen(8081, function() {
+  console.log('%s listening at %s', server.name, server.url);
+})
---
+++ static/rdio.js
@@ -0,0 +1,4 @@
+R.ready(function() {
+  console.log('rdio is ready')
+  R.player.play({source: "a171827"}) // Radiohead, The Bends
+})
