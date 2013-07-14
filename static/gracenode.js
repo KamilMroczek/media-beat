1228a43212770d086a0446a2c284d5c1352fad2d
Kamil Mroczek <kamil.mroczek@gmail.com>
2013-07-14 11:52:49 -0700

    Added some extra logging

--- static/gracenode.js
+++ static/gracenode.js
@@ -1,27 +1,3 @@
-// getLyrics = function(artist, song) {
-//   // var url = "http://lyrics.wikia.com/api.php?fmt=json";
-//   //   url += "&artist=" + encodeURIComponent(artist);
-//   //   url += "&song=" + encodeURIComponent(song);
-//   // http://lyrics.wikia.com/api.php?fmt=text&artist=Stone%20Temple%20Pilots&song=Creep
-//   
-//   var url = "http://api.musixmatch.com/ws/1.1/?";
-//   url += "apikey=3170550c6b5932906ca57a9a14d72334";
-//   url += "&q_artist=" + encodeURIComponent(artist);
-//   url += "&q_track=" + encodeURIComponent(song);
-//   
-//   alert(url);
-//   $.ajax({
-//       url: url,
-//       dataType: 'jsonp',
-//       success: function(results){
-//         alert('Worked!');
-//       },
-//       error: function(XMLHttpRequest, textStatus, errorThrown) { 
-//           alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-//       }
-//   });
-// }
-
 getTrackMood = function(cb) {
   var url = "http://mhd.gracenote.io";
   url += "/artist/" + encodeURIComponent(getCurrentArtist());
@@ -67,8 +43,3 @@ getTempoTiming = function(tempo) {
     return 750;
   }
 }
-
-// $(document).ready(function() {
-//   // getTrackMood("Nirvana", "Smells Like Teen Spirit");
-//   getTrackMood("Sandro Silva", "Epic");
-// });
--- static/images.js
+++ static/images.js
@@ -1,7 +1,7 @@
 
 window.getImages = function(artist, cb) {
   getTrackMood(function(ret) {
-    console.log('getting images for query: ' + artist)
+    console.log('getting images for query: ' + artist + ' and mood' + ret["mood"])
     $.getJSON('/images/'+artist+'/'+ret["mood"], function (data, status) {
       console.log('recieved images')
       var images = JSON.parse(data.images)
