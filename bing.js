c74935d5044d75f82cb16a18771998ba471e68fc
Saam Barati <saambarati1@gmail.com>
2013-07-24 15:36:40 -0700

    bing image filters

--- bing.js
+++ bing.js
@@ -13,7 +13,7 @@ module.exports = function(query, cb) {
   urlObj.query = {
       Query : "'"+query+"'"
     , '$format' : 'json'
-    , ImageFilters: "'Size:Large+Aspect:Wide'"
+    , ImageFilters: "'Size:Large'"
   }
 
   var options = {
